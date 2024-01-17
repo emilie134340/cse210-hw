ff9fadbf9968abc9eb4de67e30bdd3bfc351f691
Emilie Mickelsen <mickelsen.emilie@gmail.com>
2024-01-17 09:29:05 -0700

    Finished the Prep4 assignment.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        // get input for list, until 0
+        do
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while (userNumber != 0);
+        
+        // compute the average of the numbers in the list
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // get the average of the numbers in the list
+        float avg = ((float)sum) / numbers.Count; 
+        Console.WriteLine($"The average is: {avg}");
+
+        // find the largest number in the list
+        int max = numbers[0];
+        for (int i = 1; i < numbers.Count; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
