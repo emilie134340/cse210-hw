bd82b5aa1c97adccc1bb8d29ecc51ae326a77097
Emilie Mickelsen <mickelsen.emilie@gmail.com>
2024-02-20 16:07:43 -0700

    Completed the Learning04 activity.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Jones", "Math");
+        Console.WriteLine(a1.GetSummary());
+        
+        MathAssignment a2 = new MathAssignment("Rachel Smith", "Fractions", "3.4", "1-4");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Emilie Mickelsen", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
